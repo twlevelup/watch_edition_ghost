9706a073fdbb34cfd55e2c0266b72068913159fe
randbw <apbward@gmail.com>
2015-09-29 20:14:34 +1000

    added initialize function and using hard coded backbone collection for health services

--- client/src/js/pages/healthServicesPage.js
+++ client/src/js/pages/healthServicesPage.js
@@ -24,24 +24,33 @@ var HealthServicesPage = Page.extend({
     this.back();
   },
 
-  render: function() {
-
-    var healthServices = [{
+  initialize: function () {
+    var LocationsCollection = Backbone.Collection.extend({});
+    this.healthServices = new LocationsCollection();
+    var healthCentres = [{
       serviceName: 'National Services',
       contactNum: '0488888888',
       kmDist: '10km'
     },
     {
-      serviceName: 'Echo Services',
-      contactNum: '0499999999',
-      kmDist: '12km'
-    },
-    {
       serviceName: 'Help Me',
       contactNum: '0477777777',
       kmDist: '11km'
+    },
+    {
+      serviceName: 'Echo Services',
+      contactNum: '0499999999',
+      kmDist: '12km'
     }
   ];
+    this.healthServices.push(healthCentres);
+  },
+
+  render: function() {
+
+    this.healthServices.each(function () {
+      // pass model.attributes to template
+    });
 
     this.$el.html(this.template({healthServices: healthServices}));
     return this;
