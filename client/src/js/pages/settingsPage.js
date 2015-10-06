9eb2b379792b76bc64f04f973ddcd21de04db79e
Ray Tung <ray.tung97@gmail.com>
2015-10-06 19:43:42 +1100

    [Settings] remove unused code in render

--- client/src/js/pages/settingsPage.js
+++ client/src/js/pages/settingsPage.js
@@ -52,25 +52,7 @@ var SettingsPage = Page.extend({
   },
 
   render: function() {
-
-    var settings = [{
-      serviceName: 'National Services',
-      contactNum: '0488888888',
-      kmDist: '10km'
-    },
-    {
-      serviceName: 'Echo Services',
-      contactNum: '0499999999',
-      kmDist: '12km'
-    },
-    {
-      serviceName: 'Help Me',
-      contactNum: '0477777777',
-      kmDist: '11km'
-    }
-  ];
-
-    this.$el.html(this.template({Settings: settings}));
+    this.$el.html(this.template());
     return this;
 
   }
